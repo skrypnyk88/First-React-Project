8eae241054d4c1b93ad20c5518b6d32e1680e502
Dmytro Skrypnyk <skrypnyk_d@ukr.net>
2017-05-30 22:00:20 +0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header from './Header'
+import Order from './Order'
+import Inventory from './Inventory'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    const fishes = {...this.state.fishes};
+    const timeStamp = Date.now();
+    fishes[`fish-${timeStamp}`] = fish;
+    this.setState({ fishes });
+  }
+
+  render(){
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Frash sea-food market"/>
+        </div>
+        <Order/>
+        <Inventory addFish={this.addFish}/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {BrowserRouter, Match, Miss} from 'react-router'
+import NotFound from './components/NotFound'
+
+import './css/style.css';
+import App from './components/App'
+
+import StorePicker from './components/StorePicker'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match pattern="/store/:storeId" component={App}/>
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'))
